c3564c7fed8d5e04c185ba7e477327464dfe0fed
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-22 09:35:06 -0400

    Projet initial + preuve concept hibernate

---
+++ Visualisateur/src/App.java
@@ -0,0 +1,29 @@
+import java.util.Iterator;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Configuration configuration = new Configuration();
+		configuration.addClass(Exoplanete.class);
+		SessionFactory generateurSession = configuration.buildSessionFactory();
+		Session session = generateurSession.openSession();
+		
+		Iterator listeExoplanete = session.createQuery("from Exoplanete ").iterate();
+		while(listeExoplanete.hasNext())
+		{
+			Exoplanete exoplanete = (Exoplanete)listeExoplanete.next();
+			System.out.println("Planete " + exoplanete.getNom());
+		}
+				
+		session.close();
+		generateurSession.close();
+		
+		
+	}
+
+}
---
+++ Visualisateur/src/Exoplanete.java
@@ -0,0 +1,78 @@
+// id nom etoile classe masse rayon flux temperature periode distance
+public class Exoplanete {
+	
+	protected int id;
+	protected String nom;
+	protected String etoile;
+	protected String classe;
+	protected String masse;
+	protected String rayon;
+	protected String flux;
+	protected String temperature;
+	protected String periode;
+	protected String distance;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getEtoile() {
+		return etoile;
+	}
+	public void setEtoile(String etoile) {
+		this.etoile = etoile;
+	}
+	public String getClasse() {
+		return classe;
+	}
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	public String getMasse() {
+		return masse;
+	}
+	public void setMasse(String masse) {
+		this.masse = masse;
+	}
+	public String getRayon() {
+		return rayon;
+	}
+	public void setRayon(String rayon) {
+		this.rayon = rayon;
+	}
+	public String getFlux() {
+		return flux;
+	}
+	public void setFlux(String flux) {
+		this.flux = flux;
+	}
+	public String getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+	public String getPeriode() {
+		return periode;
+	}
+	public void setPeriode(String periode) {
+		this.periode = periode;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	
+	
+
+}
