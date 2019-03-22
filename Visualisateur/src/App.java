07e92a5fa920e6073460e7a44ad78ffea2d3d0da
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-22 09:44:06 -0400

    Factoriser le code dans le DAO

--- Visualisateur/src/App.java
+++ Visualisateur/src/App.java
@@ -1,33 +1,9 @@
-import java.util.Iterator;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class App {
 
 	public static void main(String[] args) {
 
 		ExoplaneteDAO exoplaneteDAO = new ExoplaneteDAO();
-		exoplaneteDAO.listerExoplanete();
-		
-		
-		Configuration configuration = new Configuration();
-		configuration.addClass(Exoplanete.class);
-		SessionFactory generateurSession = configuration.buildSessionFactory();
-		Session session = generateurSession.openSession();
-		
-		Iterator listeExoplanete = session.createQuery("from Exoplanete ").iterate();
-		while(listeExoplanete.hasNext())
-		{
-			Exoplanete exoplanete = (Exoplanete)listeExoplanete.next();
-			System.out.println("Planete " + exoplanete.getNom());
-		}
-				
-		session.close();
-		generateurSession.close();
-		
-		
+		exoplaneteDAO.listerExoplanete();		
 	}
 
 }
--- Visualisateur/src/ExoplaneteDAO.java
+++ Visualisateur/src/ExoplaneteDAO.java
@@ -1,11 +1,31 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class ExoplaneteDAO {
 	
 	public List<Exoplanete> listerExoplanete()
 	{
 		List<Exoplanete> listeExoplanete = new ArrayList<Exoplanete>();
+		
+		Configuration configuration = new Configuration();
+		configuration.addClass(Exoplanete.class);
+		SessionFactory generateurSession = configuration.buildSessionFactory();
+		Session session = generateurSession.openSession();
+		
+		Iterator visiteurExoplanete = session.createQuery("from Exoplanete ").iterate();
+		while(visiteurExoplanete.hasNext())
+		{
+			Exoplanete exoplanete = (Exoplanete)visiteurExoplanete.next();
+			System.out.println("Planete " + exoplanete.getNom());
+		}
+				
+		session.close();
+		generateurSession.close();
 		
 		return listeExoplanete;
 	}
