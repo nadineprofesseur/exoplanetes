47f6bfae33b8c289c06b4d88bbc115a496fba65e
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-03-27 13:52:40 -0400

    Test affichage

--- Visualisateur/src/PageListeExoplanete.java
+++ Visualisateur/src/PageListeExoplanete.java
@@ -2,11 +2,16 @@ import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TextArea;
 
 public class PageListeExoplanete extends Page {
 
 	public PageListeExoplanete() throws IOException {
 		super(FXMLLoader.load(PageListeExoplanete.class.getResource("liste-exoplanetes.fxml")));
+		
+		TextArea espaceTexte = (TextArea) this.lookup("#liste-exoplanetes");
+		espaceTexte.appendText("coucou");
 	}
+	
 
 }
