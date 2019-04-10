49d7f5d012249421cfceb8a8c5116b0d8fd05cf9
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-04-10 13:28:31 -0400

    Recuperation des donnees du formulaire

--- Editeur/src/editeur/vue/PageAjouterExoplanete.java
+++ Editeur/src/editeur/vue/PageAjouterExoplanete.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import editeur.modele.Exoplanete;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class PageAjouterExoplanete extends Page {
 
@@ -12,7 +14,17 @@ public class PageAjouterExoplanete extends Page {
 	}
 	
 	public Exoplanete lireExoplanete()
-	{
-		return null;
+	{		
+		Exoplanete exoplanete = new Exoplanete();
+	
+		TextField champsNom = (TextField) this.lookup("#champs-nom");
+		String nom = champsNom.getText();
+		exoplanete.setNom(nom);
+		TextField champsEtoile = (TextField) this.lookup("#champs-etoile");
+		String etoile = champsNom.getText();
+		exoplanete.setNom(etoile);
+		
+		System.out.println("Nouvelle planete " + nom + " dans le soleil " + nom);
+		return exoplanete;
 	}
 }
