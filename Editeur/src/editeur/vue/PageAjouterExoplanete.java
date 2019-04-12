3413348bd9fc2a75b0beb36fbb35201b304c44d3
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-04-12 08:08:24 -0400

    Tous les champs dans les formulaires

--- Editeur/src/editeur/vue/PageAjouterExoplanete.java
+++ Editeur/src/editeur/vue/PageAjouterExoplanete.java
@@ -23,8 +23,24 @@ public class PageAjouterExoplanete extends Page {
 		TextField champsEtoile = (TextField) this.lookup("#champs-etoile");
 		exoplanete.setEtoile(champsEtoile.getText());
 		champsEtoile.setText("");
-
-		// TODO lire et vider tous les champs
+		TextField masseEtoile = (TextField) this.lookup("#champs-masse");
+		exoplanete.setEtoile(masseEtoile.getText());
+		masseEtoile.setText("");
+		TextField rayonEtoile = (TextField) this.lookup("#champs-rayon");
+		exoplanete.setEtoile(rayonEtoile.getText());
+		rayonEtoile.setText("");
+		TextField fluxEtoile = (TextField) this.lookup("#champs-flux");
+		exoplanete.setEtoile(fluxEtoile.getText());
+		fluxEtoile.setText("");
+		TextField temperatureEtoile = (TextField) this.lookup("#champs-temperature");
+		exoplanete.setEtoile(temperatureEtoile.getText());
+		temperatureEtoile.setText("");
+		TextField periodeEtoile = (TextField) this.lookup("#champs-periode");
+		exoplanete.setEtoile(periodeEtoile.getText());
+		periodeEtoile.setText("");
+		TextField distanceEtoile = (TextField) this.lookup("#champs-distance");
+		exoplanete.setEtoile(distanceEtoile.getText());
+		distanceEtoile.setText("");
 		
 		//System.out.println("Nouvelle planete " + nom + " dans le soleil " + nom);
 		return exoplanete;
--- Editeur/src/editeur/vue/PageEditerExoplanete.java
+++ Editeur/src/editeur/vue/PageEditerExoplanete.java
@@ -22,8 +22,24 @@ public class PageEditerExoplanete extends Page {
 		TextField champsEtoile = (TextField) this.lookup("#champs-etoile");
 		exoplanete.setEtoile(champsEtoile.getText());
 		champsEtoile.setText("");
-
-		// TODO lire et vider tous les champs
+		TextField masseEtoile = (TextField) this.lookup("#champs-masse");
+		exoplanete.setEtoile(masseEtoile.getText());
+		masseEtoile.setText("");
+		TextField rayonEtoile = (TextField) this.lookup("#champs-rayon");
+		exoplanete.setEtoile(rayonEtoile.getText());
+		rayonEtoile.setText("");
+		TextField fluxEtoile = (TextField) this.lookup("#champs-flux");
+		exoplanete.setEtoile(fluxEtoile.getText());
+		fluxEtoile.setText("");
+		TextField temperatureEtoile = (TextField) this.lookup("#champs-temperature");
+		exoplanete.setEtoile(temperatureEtoile.getText());
+		temperatureEtoile.setText("");
+		TextField periodeEtoile = (TextField) this.lookup("#champs-periode");
+		exoplanete.setEtoile(periodeEtoile.getText());
+		periodeEtoile.setText("");
+		TextField distanceEtoile = (TextField) this.lookup("#champs-distance");
+		exoplanete.setEtoile(distanceEtoile.getText());
+		distanceEtoile.setText("");
 		
 		//System.out.println("Nouvelle planete " + nom + " dans le soleil " + nom);
 		return exoplanete;
@@ -35,6 +51,18 @@ public class PageEditerExoplanete extends Page {
 		champsNom.setText(exoplanete.getNom());
 		TextField champsEtoile = (TextField) this.lookup("#champs-etoile");
 		champsEtoile.setText(exoplanete.getEtoile());
+		TextField masseEtoile = (TextField) this.lookup("#champs-masse");
+		masseEtoile.setText(exoplanete.getEtoile());
+		TextField rayonEtoile = (TextField) this.lookup("#champs-rayon");
+		rayonEtoile.setText(exoplanete.getEtoile());
+		TextField fluxEtoile = (TextField) this.lookup("#champs-flux");
+		fluxEtoile.setText(exoplanete.getEtoile());
+		TextField temperatureEtoile = (TextField) this.lookup("#champs-temperature");
+		temperatureEtoile.setText(exoplanete.getEtoile());
+		TextField periodeEtoile = (TextField) this.lookup("#champs-periode");
+		periodeEtoile.setText(exoplanete.getEtoile());
+		TextField distanceEtoile = (TextField) this.lookup("#champs-distance");
+		distanceEtoile.setText(exoplanete.getEtoile());
 		
 		// TODO afficher tous les champs
 		
