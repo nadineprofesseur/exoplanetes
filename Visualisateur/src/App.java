fb38e17703a0d9c02088c6fcdace537e11c340e3
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-04-18 15:13:15 -0400

    Exemple appel Recherches de planetes avec design pattern Template

--- Visualisateur/src/App.java
+++ Visualisateur/src/App.java
@@ -1,12 +1,24 @@
 import java.util.List;
 
+import visualisateur.action.AlgorithmeRecherche;
+import visualisateur.action.AlgorithmeRechercheProfesseurTournesol;
+import visualisateur.action.AlgorithmeRechercheProfesseurVadeboncoeur;
+import visualisateur.donnee.ExoplaneteDAO;
+import visualisateur.modele.Exoplanete;
 import visualisateur.vue.NavigateurDePages;
 
 public class App {
 
 	public static void main(String[] parametres) {
 		
-		NavigateurDePages.launch(NavigateurDePages.class, parametres);
+		//NavigateurDePages.launch(NavigateurDePages.class, parametres);
+		List<Exoplanete> listeExoplanetes;
+		
+		AlgorithmeRecherche algorithmeTournesol = new AlgorithmeRechercheProfesseurTournesol(ExoplaneteDAO.getInstance().listerExoplanete());
+		listeExoplanetes = algorithmeTournesol.executer();
+		// for + afficher
+		AlgorithmeRecherche algorithmeVadeboncoeur = new AlgorithmeRechercheProfesseurVadeboncoeur(ExoplaneteDAO.getInstance().listerExoplanete());
+		listeExoplanetes = algorithmeTournesol.executer();
 		
 	}
 
